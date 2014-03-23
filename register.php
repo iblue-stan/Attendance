064b7de3b43cd9f23a1712676271de8463d6a8ec
ibluestan <kevilan@gmail.com>
2014-03-23 15:46:26 +0800

    update20140323

--- register.php
+++ register.php
@@ -1,44 +1,44 @@
 <?php session_start(); ?>
 <!doctype html>
 <html lang="en">
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
-	<title>untitled</title>
-	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-	<script src="js/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-</head>
-<body>
-<?php  include_once("fixed.php");  ?>
-<br>
-<br>
-<br>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
+        <title>untitled</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <?php include_once("fixed.php"); ?>
+        <br />
+        <br />
+        <br />
 
-<form name="form" method="post" action="register_finish.php">
-	<table class="table" >
-	<tr><td width='15%'>姓名</td><td ><input type="text" class='form-control' name="user_name"></td></tr>
-	<tr><td>手機</td><td><input type="text" class='form-control' name="id" ></td></tr>
-	<tr><td>密碼</td><td><input type="password" class='form-control' name="pw" ></td></tr>
-	<tr><td>再一次輸入密碼</td><td><input type="password" class='form-control' name="pw2" ></td></tr>
-	<tr><td>權限</td><td>
-		<select name="AA" class='form-control' size="1"> 
-			<option value="2">一般使用者</option>
-			<option value="1">管理員</option>
-		</select>
-	</td></tr>
-	<tr><td>輸入部門：</td><td>
-		<select class='form-control' name="pick_class" >
-			<option value="1">資訊部</option>
-			<option value="2">市場部</option>
-			<option value="3">行銷部</option>
-		</select>
-	</td></tr>
-	<tr><td colspan=4>
-        <input class="btn btn-default" type="submit" value="送出">
-        <input class="btn btn-default" type="reset" value="重設">
-	</td></tr>
-	</table>
-</form>
+        <form name="form" method="post" action="register_finish.php">
+            <table class="table" >
+                <tr><td width='15%'>姓名</td><td ><input type="text" class='form-control' name="user_name"></td></tr>
+                <tr><td>手機</td><td><input type="text" class='form-control' name="id" ></td></tr>
+                <tr><td>密碼</td><td><input type="password" class='form-control' name="pw" ></td></tr>
+                <tr><td>再一次輸入密碼</td><td><input type="password" class='form-control' name="pw2" ></td></tr>
+                <tr><td>權限</td><td>
+                        <select name="AA" class='form-control' size="1"> 
+                            <option value="2">一般使用者</option>
+                            <option value="1">管理員</option>
+                        </select>
+                    </td></tr>
+                <tr><td>輸入部門：</td><td>
+                        <select class='form-control' name="pick_class" >
+                            <option value="1">資訊部</option>
+                            <option value="2">市場部</option>
+                            <option value="3">行銷部</option>
+                        </select>
+                    </td></tr>
+                <tr><td colspan=4>
+                        <input class="btn btn-default" type="submit" value="送出">
+                        <input class="btn btn-default" type="reset" value="重設">
+                    </td></tr>
+            </table>
+        </form>
 
-</body>
+    </body>
 </html>
--- update.php
+++ update.php
@@ -1,57 +1,68 @@
 <?php session_start(); ?>
 <!doctype html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Modify/Update</title>
-  <!-- Bootstrap -->
-  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen"></head>
-<body>
-  <h5>
-<a class="btn btn-primary" href='admin.php' role='botton'>Back</a>
-</h5>
+    <head>
+        <meta charset="UTF-8">
+        <title>Modify/Update</title>
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen"></head>
+    <body>
+        <h5><a class="btn btn-primary" href='admin.php' role='botton'>Back</a></h5>
+        <?php
+        include_once("fixed.php");
+        include("mysql_connect.inc.php");
+        $user_phone = filter_input(INPUT_GET, ('user_phone'));
+        $sql = "SELECT * FROM user where user_phone ='$user_phone' ";
+        $r = mysql_query($sql);
 
-	
-  <?php 
-include_once("fixed.php");
-include("mysql_connect.inc.php");
-$user_phone = $_GET['user_phone'];
-
-$sql = "SELECT * FROM user where user_phone ='$user_phone' ";
-$r = mysql_query($sql);
-
-while ($row = mysql_fetch_assoc($r)){
-
-?>
-
-<form name=form action='update_finish.php' method='POST'>
-<table class='table' >
-<tr><td>Phone</td><td><input type='text' class='form-control' value= '<?php echo  $row['user_phone'];?>' name='phone'></td></tr>
-<tr><td>Name</td><td><input type='text' class='form-control' value= '<?php echo  $row['user_name'];?>' name='name'></td></tr>
-<tr><td>Password</td><td><input type='text' class='form-control' value= '<?php echo  $row['user_pwd'];?>' name='pwd'></td></tr>
-<tr><td>Class</td>
-</td><td><select name="class" class='form-control' >
-  <option value="1" <?php if($row['user_class']==1) echo "selected"; ?>>資訊部</option>
-  <option value="2" <?php if($row['user_class']==2) echo "selected"; ?>>市場部</option>
-  <option value="3" <?php if($row['user_class']==3) echo "selected"; ?>>行銷部</option>
-</select>
-</td></tr>
-
-<input type="hidden" value=<?php echo $user_phone; ?> name='$phone'>
-
-      <tr><td colspan=4><center>
-        <input class="btn btn-default" type="submit" value="送出">
-        <input class="btn btn-default" type="reset" value="重設">
-      </center></td></tr>
-
-    </table>
-  </form>
-
-</h3>
-
-<?php 
-}
- ?>
- 
-</body>
+        while ($row = mysql_fetch_assoc($r)) {
+            ?>
+            <div class="row">
+                <form class="form-horizontal" role="form" name=form action='update_finish.php' method='POST'>
+                    <table class='table' >
+                        <tr>
+                            <td><label class="control-label col-sm-3" for="inputSuccess3">Phone</label>
+                                <div class="col-xs-3">
+                                    <input type='text' placeholder=".col-xs-3" class='form-control' value= '<?php echo $row['user_phone']; ?>' name='phone'>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label class="control-label col-sm-3" for="inputSuccess3">Name</label>
+                                <div class="col-xs-3">
+                                    <input type='text' class='form-control' value= '<?php echo $row['user_name']; ?>' name='name'>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label class="control-label col-sm-3" for="inputSuccess3">Password</label>
+                                <div class="col-xs-3"><input type='text' class='form-control' value= '<?php echo $row['user_pwd']; ?>' name='pwd'></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label class="control-label col-sm-3" for="inputSuccess3">Class</label>
+                                <div class="col-xs-3"><select name="class" class='form-control' ></dvi>
+                                        <option value="1" <?php if ($row['user_class'] == 1) echo "selected"; ?>>資訊部</option>
+                                        <option value="2" <?php if ($row['user_class'] == 2) echo "selected"; ?>>市場部</option>
+                                        <option value="3" <?php if ($row['user_class'] == 3) echo "selected"; ?>>行銷部</option>
+                                    </select>
+                            </td>
+                        </tr>
+                        <input type="hidden" value=<?php echo $user_phone; ?> name='$phone'>
+                        <tr>
+                            <td colspan=4>
+                        <center>
+                            <input class="btn btn-default" type="submit" value="送出">
+                            <input class="btn btn-default" type="reset" value="重設">
+                        </center>
+                        </td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+            <?php
+        }
+        ?>
+    </body>
 </html>
