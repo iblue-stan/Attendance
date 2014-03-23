38e37499e43b1dfebde05bc62258d58cf2e46e8e
ibluestan <kevilan@gmail.com>
2014-03-23 16:55:00 +0800

    Update20140323

--- index.php
+++ index.php
@@ -1,36 +1,46 @@
 <html>
-<head>
-<title>會員登入</title>
-<meta http-equiv = "content-Type" content="text/html; charset=utf-8">
-<script type="text/javascript" src="js/login.js"></script>
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/signin.css" rel="stylesheet">
-<body>
+    <head>
+        <title>會員登入</title>
+        <meta http-equiv = "content-Type" content="text/html; charset=utf-8">
+        <script type="text/javascript" src="js/login.js"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/signin.css" rel="stylesheet">
+    <body>
 
-<?php
-if(empty($_SESSION['username'])){
-?>
+        <?php
+        session_start();
+        if(!empty($_SESSION["username"])){
+            $user_permission = $_SESSION["user_permission"];
+            $user_name = $_SESSION["username"];
+        }
 
- <div class="container">
+        if (empty($user_name)) {
+            ?>
+            <div class="container">
+                <form name="form" method="post" action="connect.php" class="form-signin" role="form">
+                    <h2 class="form-signin-heading"><center>會員登入</center></h2>
+                    <input type="text" name="id" class="form-control" placeholder="User ID" required autofocus>
+                    <input type="password" name="pw" id="pw" class="form-control" placeholder="Password" required>
+                    <label class="checkbox">
+                        <input type="checkbox" value="remember-me"> 記住我
+                    </label>
+                    <label class="checkbox">
+                        <input type="checkbox" id="password" name="password" onclick="pwdshow(this);">顯示/隱藏密碼</input> |
+                        <a href="mailto:admin@gmail.com?subject=密碼重設" style="text-decoration:none;">忘記密碼?</a>
+                    </label>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+                </form>
+            </div>	
 
-      <form name="form" method="post" action="connect.php" class="form-signin" role="form">
-        <h2 class="form-signin-heading">會  員  登  入</h2>
-        <input type="text" name="id" class="form-control" placeholder="User ID" required autofocus>
-        <input type="password" name="pw" id="pw" class="form-control" placeholder="Password" required>
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> 記住我
-        </label>
-        <label class="checkbox">
-		<input type="checkbox" id="password" name="password" onclick="pwdshow(this);">顯示/隱藏密碼</input> |
-		<a href="mailto:admin@gmail.com?subject=密碼重設" style="text-decoration:none;">忘記密碼?</a>
-    </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
-      </form>
-    </div>	
-	
-<?php
-}
-?>
+            <?php
+        } else {
+            if ($user_permission == 1) {
+                header('refresh:1;url=admin.php');
+            } elseif ($user_permission == 2) {
+                header('refresh:1;url=member.php');
+            }
+        }
+        ?>
 
-</body>
+    </body>
 </html>
--- logout.php
+++ logout.php
@@ -1,9 +1,11 @@
-<?php session_start(); 
-?>
+<?php session_start();?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
 //將session清空
 unset($_SESSION['username']);
+unset($_SESSION['loginname']);
+unset($_SESSION['id']);
+unset($_SESSION['user_permission']);
 echo '登出中......';
 echo '<meta http-equiv=REFRESH CONTENT=0;url=index.php>';
 ?>
